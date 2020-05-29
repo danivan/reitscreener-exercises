d10a623247f179f6c6b7dc651750ee5cf4edb0e8
Dan Ivan Sentillas <dan.ivan.sentilllas@gmail.com>
2020-05-29 11:10:40 +0800

    fix(procedures.sql): fix syntax error in the file

--- exercise-01/procedures.sql
+++ exercise-01/procedures.sql
@@ -2,24 +2,32 @@ CREATE OR REPLACE FUNCTION calculateSharePriceNAV()
   RETURNS trigger AS
 $BODY$
 DECLARE
-	auditedNAVPerUnit FLOAT := SELECT "auditedNAVPerUnit" FROM "AnnualReport" ORDER BY "announcementDate" DESC LIMIT 1
+	auditedNAVPerUnit FLOAT;
 BEGIN
+    SELECT "auditedNAVPerUnit" INTO auditedNAVPerUnit FROM "AnnualReport" 
+	WHERE "reitId" = NEW.reitId ORDER BY "announcementDate" DESC LIMIT 1;
+
 	INSERT INTO "SharePriceNav"(reitId,date,pricePerNAVPerUnit)
 	VALUES(OLD.reitId, CURRENT_DATE, NEW.close / auditedNAVPerUnit);
 
 	RETURN NEW;
 END;
 $BODY$
+LANGUAGE PLPGSQL;;
 
 CREATE OR REPLACE FUNCTION calculateSharePriceYield()
   RETURNS trigger AS
 $BODY$
 DECLARE
-	declaredDPU FLOAT := SELECT "declaredDPU" FROM "AnnualReport" ORDER BY "announcementDate" DESC LIMIT 1;
+	declaredDPU FLOAT;
 BEGIN
+    SELECT "declaredDPU" INTO declaredDPU FROM "AnnualReport" 
+	WHERE "reitId" = NEW.reitId ORDER BY "announcementDate" DESC LIMIT 1;
+
 	INSERT INTO "SharePriceYield"(reitId,date,yield)
 	VALUES(OLD.reitId, CURRENT_DATE, declaredDPU / NEW.close);
 
 	RETURN NEW;
 END;
 $BODY$
+LANGUAGE PLPGSQL;
