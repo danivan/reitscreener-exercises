b6caf186fe80bc48224f69568283abcbf3608470
Dan Ivan Sentillas <dan.ivan.sentilllas@gmail.com>
2020-05-29 17:51:49 +0800

    fix(exercise-01): fix ambiguous column names used in the functions and triggers

--- exercise-01/procedures.sql
+++ exercise-01/procedures.sql
@@ -1,43 +1,43 @@
-DROP FUNCTION calculateSharePriceNAV;
+DROP FUNCTION IF EXISTS "calculateSharePriceNAV" CASCADE;
 
-CREATE OR REPLACE FUNCTION calculateSharePriceNAV()
+CREATE OR REPLACE FUNCTION "calculateSharePriceNAV"()
   RETURNS trigger AS
 $BODY$
 DECLARE
-	auditedNAVPerUnit FLOAT;
+	"auditedNAVPerUnit" FLOAT;
 BEGIN
 	IF NEW.close = 0 THEN
-		RETURN;
+		RETURN NEW;
 	END IF;
 
-    SELECT "auditedNAVPerUnit" INTO auditedNAVPerUnit FROM "AnnualReport" 
-	WHERE "reitId" = NEW."reitId" ORDER BY "announcementDate" DESC LIMIT 1;
+    SELECT ar."auditedNAVPerUnit" INTO "auditedNAVPerUnit" FROM "AnnualReport" AS ar
+	WHERE ar."reitId" = NEW."reitId" ORDER BY ar."announcementDate" DESC LIMIT 1;
 
 	INSERT INTO "SharePriceNAV"("reitId",date,"pricePerNAVPerUnit")
-	VALUES(NEW."reitId", CURRENT_DATE, NEW.close / auditedNAVPerUnit);
+	VALUES(NEW."reitId", NEW.date, NEW.close / "auditedNAVPerUnit");
 
 	RETURN NEW;
 END;
 $BODY$
 LANGUAGE PLPGSQL;
 
-DROP FUNCTION calculateSharePriceYield;
+DROP FUNCTION IF EXISTS "calculateSharePriceYield" CASCADE;
 
-CREATE OR REPLACE FUNCTION calculateSharePriceYield()
+CREATE OR REPLACE FUNCTION "calculateSharePriceYield"()
   RETURNS trigger AS
 $BODY$
 DECLARE
-	declaredDPU FLOAT;
+	"declaredDPU" FLOAT;
 BEGIN
 	IF NEW.close = 0 THEN
-		RETURN;
+		RETURN NEW;
 	END IF;
 
-    SELECT "declaredDPU" INTO declaredDPU FROM "AnnualReport" 
-	WHERE "reitId" = NEW."reitId" ORDER BY "announcementDate" DESC LIMIT 1;
+    SELECT ar."declaredDPU" INTO "declaredDPU" FROM "AnnualReport" AS ar
+	WHERE ar."reitId" = NEW."reitId" ORDER BY ar."announcementDate" DESC LIMIT 1;
 
 	INSERT INTO "SharePriceYield"("reitId",date,yield)
-	VALUES(NEW."reitId", CURRENT_DATE, declaredDPU / NEW.close);
+	VALUES(NEW."reitId", NEW.date, "declaredDPU" / NEW.close);
 
 	RETURN NEW;
 END;
--- exercise-01/triggers.sql
+++ exercise-01/triggers.sql
@@ -1,5 +1,5 @@
-CREATE TRIGGER calculateSharePriceNAVTrigger AFTER INSERT OR UPDATE ON "SharePrice"
-FOR EACH ROW EXECUTE PROCEDURE calculateSharePriceNAV();
+CREATE TRIGGER "calculateSharePriceNAVTrigger" AFTER INSERT OR UPDATE ON "SharePrice"
+FOR EACH ROW EXECUTE PROCEDURE "calculateSharePriceNAV"();
 
-CREATE TRIGGER calculateSharePriceYieldTrigger AFTER INSERT OR UPDATE ON "SharePrice"
-FOR EACH ROW EXECUTE PROCEDURE calculateSharePriceYield();
+CREATE TRIGGER "calculateSharePriceYieldTrigger" AFTER INSERT OR UPDATE ON "SharePrice"
+FOR EACH ROW EXECUTE PROCEDURE "calculateSharePriceYield"();
