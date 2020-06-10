2c3c4177658918964c07f5cbef00e35c42c2da21
Dan Ivan Sentillas <dan.ivan.sentillas@gmail.com>
2020-06-10 19:15:16 +0800

    fix(exercise-01): fix procedures
    add validation to check if the initial select query is returning a row

--- exercise-01/procedures.sql
+++ exercise-01/procedures.sql
@@ -5,13 +5,21 @@ CREATE OR REPLACE FUNCTION "calculateSharePriceNAV"()
 $BODY$
 DECLARE
 	"auditedNAVPerUnit" FLOAT;
+	"rowCount" INT;
 BEGIN
 	IF NEW.close = 0 THEN
 		RETURN NEW;
 	END IF;
 
     SELECT ar."auditedNAVPerUnit" INTO "auditedNAVPerUnit" FROM "AnnualReport" AS ar
-	WHERE ar."reitId" = NEW."reitId" ORDER BY ar."announcementDate" DESC LIMIT 1;
+	WHERE ar."reitId" = NEW."reitId" AND ar."announcementDate" <= NEW.date 
+	ORDER BY ar."announcementDate" DESC LIMIT 1;
+
+	GET DIAGNOSTICS "rowCount" := ROW_COUNT;
+
+	IF "rowCount" = 0 THEN
+		RETURN NEW;
+	END IF;
 
 	INSERT INTO "SharePriceNAV"("reitId",date,"pricePerNAVPerUnit")
 	VALUES(NEW."reitId", NEW.date, NEW.close / "auditedNAVPerUnit");
@@ -28,13 +36,21 @@ CREATE OR REPLACE FUNCTION "calculateSharePriceYield"()
 $BODY$
 DECLARE
 	"declaredDPU" FLOAT;
+	"rowCount" INT;
 BEGIN
 	IF NEW.close = 0 THEN
 		RETURN NEW;
 	END IF;
 
     SELECT ar."declaredDPU" INTO "declaredDPU" FROM "AnnualReport" AS ar
-	WHERE ar."reitId" = NEW."reitId" ORDER BY ar."announcementDate" DESC LIMIT 1;
+	WHERE ar."reitId" = NEW."reitId" AND ar."announcementDate" <= NEW.date 
+	ORDER BY ar."announcementDate" DESC LIMIT 1;
+
+	GET DIAGNOSTICS "rowCount" := ROW_COUNT;
+
+	IF "rowCount" = 0 THEN
+		RETURN NEW;
+	END IF;
 
 	INSERT INTO "SharePriceYield"("reitId",date,yield)
 	VALUES(NEW."reitId", NEW.date, "declaredDPU" / NEW.close);
@@ -43,3 +59,6 @@ BEGIN
 END;
 $BODY$
 LANGUAGE PLPGSQL;
+
+--run triggers
+\i triggers.sql
