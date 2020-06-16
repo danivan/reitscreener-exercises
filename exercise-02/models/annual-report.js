73aa1c7e43803b8ddd500e23283356bf2501e385
Dan Ivan Sentillas <dan.ivan.sentillas@gmail.com>
2020-06-17 01:23:18 +0800

    fix(exercise-02): add missing columns for reit and annual-report models

--- exercise-02/models/annual-report.js
+++ exercise-02/models/annual-report.js
@@ -3,10 +3,12 @@ import sequelize from '../lib/sequelize';
 
 const AnnualReportModel = sequelize.define('AnnualReport', {
   reitId: DataTypes.INTEGER,
+  frequency: DataTypes.STRING,
   year: DataTypes.INTEGER,
   announcementDate: DataTypes.DATE,
   auditedNAVPerUnit: DataTypes.FLOAT,
   declaredDPU: DataTypes.FLOAT,
+  link: DataTypes.STRING,
 }, { tableName: 'AnnualReport', timestamps: false });
 
 AnnualReportModel.removeAttribute('id');
--- exercise-02/models/reit.js
+++ exercise-02/models/reit.js
@@ -2,6 +2,7 @@ import DataTypes from 'sequelize/lib/data-types';
 import sequelize from '../lib/sequelize';
 
 const ReitModel = sequelize.define('Reit', {
+  reitId: DataTypes.INTEGER,
   name: DataTypes.STRING,
   stockCode: DataTypes.STRING,
   exchange: DataTypes.STRING,
