abb43d204129bc23b9ef3fead10f2aab3855aeff
Dan Ivan Sentillas <dan.ivan.sentillas@gmail.com>
2020-06-19 04:38:26 +0800

    fix(exercise-02): add column constraints for reitId
    fixes TypeError: Cannot read property 'toUpperCase' of undefined when starting apollo server

--- exercise-02/models/reit.js
+++ exercise-02/models/reit.js
@@ -2,7 +2,12 @@ import DataTypes from 'sequelize/lib/data-types';
 import sequelize from '../lib/sequelize';
 
 const ReitModel = sequelize.define('Reit', {
-  reitId: DataTypes.INTEGER,
+  reitId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
   name: DataTypes.STRING,
   stockCode: DataTypes.STRING,
   exchange: DataTypes.STRING,
@@ -12,5 +17,9 @@ const ReitModel = sequelize.define('Reit', {
 }, { tableName: 'Reit', timestamps: false });
 
 ReitModel.removeAttribute('id');
+
+ReitModel.associate = (models) => {
+  ReitModel.hasOne(models.Watchlist, { foreignKey: 'reitId' });
+};
 
 export default ReitModel;
