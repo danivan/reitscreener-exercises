cc6ccca06ed19cb2239bf326c46c9f14deb92c41
Dan Ivan Sentillas <dan.ivan.sentillas@gmail.com>
2020-06-19 04:31:24 +0800

    refactor(exercise-02): re-arrange variable declaration in server

--- exercise-02/src/api/graphql/index.js
+++ exercise-02/src/api/graphql/index.js
@@ -1,23 +1,21 @@
 import Koa from 'koa'; // koa@2
-import { ApolloServer, gql } from 'apollo-server-koa';
+import { ApolloServer } from 'apollo-server-koa';
 import { importSchema } from 'graphql-import';
 import { fileLoader, mergeResolvers } from 'merge-graphql-schemas';
 import path from 'path';
 import db from '../../../models/index';
 
+const typeDefs = importSchema(path.join(__dirname, '../../../../schema.gql'));
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, '/resolvers'), { recursive: true }));
+
+const server = new ApolloServer({ typeDefs, resolvers, context: { db } });
+
+const app = new Koa();
+server.applyMiddleware({ app });
+
 export default {
-  start: (port = 4000) => {
-    const typeDefs = importSchema(path.join(__dirname, '../../../../schema.gql'));
-    const resolvers = mergeResolvers(fileLoader(path.join(__dirname, '/resolvers'), { recursive: true }));
-
-    const server = new ApolloServer({ typeDefs, resolvers, context: { db } });
-
-    const app = new Koa();
-    server.applyMiddleware({ app });
-
-    app.listen({ port }, () => {
-      // eslint-disable-next-line no-console
-      console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`);
-    });
-  },
+  start: (port = 4000) => app.listen({ port }, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`);
+  }),
 };
